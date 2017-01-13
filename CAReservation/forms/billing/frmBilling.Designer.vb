5e7d50703b202bc6453992456fad32fa39d071d6
marz3l <marlon_tejol@yahoo.com>
2017-01-13 15:53:45 +0800

    MT: Added new button to billing UI 2017-JAN-13

--- CAReservation/forms/billing/frmBilling.Designer.vb
+++ CAReservation/forms/billing/frmBilling.Designer.vb
@@ -22,8 +22,29 @@ Partial Class frmBilling
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(443, 185)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmBilling
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(642, 400)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "frmBilling"
         Me.Text = "frmBilling"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
